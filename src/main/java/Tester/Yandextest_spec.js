148cb51047f8e1c3fbbec79e94f5e3a4a2edf8f6
Dineesha <dinsuraw@gmail.com>
2016-06-02 17:26:18 +0530

    Add more test cases of yandex authentication

--- src/main/java/Tester/Yandextest_spec.js
+++ src/main/java/Tester/Yandextest_spec.js
@@ -9,6 +9,7 @@ frisby.create('Verify working language list')
     .get('https://translate.yandex.net/api/v1.5/tr.json/getLangs?key=trnsl.1.1.20160311T110648Z.2843309257351b77.503eb0ab4fee6d8e09936972b4bc73810e4b12b4&ui=en')
     .expectStatus(200)
     .inspectBody()
+    .inspectJSON()
     .expectHeaderContains('Content-Type', 'application/json')
     .toss()
 
@@ -18,9 +19,62 @@ frisby.create('verify translation is success')
     .expectHeaderContains('Content-Type', 'application/json')
     .expectHeader('Content-Type', 'application/json; charset=utf-8')
     .expectJSON(
-    {
-        "code":200,
+    {"code":200,
         "lang":"en-fr",
         "text":["arbre"]}
 )
+    .inspectJSON()
+
+    .toss()
+
+frisby.create('verify translation variations')
+    .get('https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20160314T055210Z.609ee89149686a99.22e2a4c8f48279cba64fe814237ef790791fd2c5&text=&lang=en-fr')
+    .inspectBody()
+    .expectHeaderContains('Content-Type', 'application/json')
+    .expectHeader('Content-Type', 'application/json; charset=utf-8')
+    .expectJSON(
+    {"code":200,
+        "lang":"en-fr",
+        "text":[""]}
+)
+    .inspectJSON()
+
+    .toss()
+
+frisby.create('verify translation variations')
+    .get('https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20160314T055210Z.609ee89149686a99.22e2a4c8f48279cba64fe814237ef790791fd2c5&text=tree&lang=en-')
+    .inspectBody()
+    .expectHeaderContains('Content-Type', 'application/json')
+    .expectHeader('Content-Type', 'application/json; charset=utf-8')
+    .expectJSONTypes(
+    {"code":501,
+        "message":"The spe"}
+
+)
+    .inspectJSON()
+
+    .toss()
+
+frisby.create('verify translation variations')
+    .get('https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20160314T055210Z.609ee89149686a99.22e2a4c8f48279cba64fe814237ef790791fd2c5&text=big tree&lang=en-si')
+    .inspectBody()
+    .expectHeaderContains('Content-Type', 'application/json')
+    .expectHeader('Content-Type', 'application/json; charset=utf-8')
+    .expectJSON(
+    {"code":200,"lang":"en-si","text":["විශාල ගස"]}
+)
+    .inspectJSON()
+
+    .toss()
+
+frisby.create('verify translation variations')
+    .get('https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20160314T055210Z.609ee89149686a99.22e2a4c8f48279cba64fe814237ef790791fd2c5&text=විශාල ගස&lang=si-en')
+    .inspectBody()
+    .expectHeaderContains('Content-Type', 'application/json')
+    .expectHeader('Content-Type', 'application/json; charset=utf-8')
+    .expectJSONTypes(
+    {"code":200,"lang":"si-en","text":["big tree"]}
+)
+    .inspectJSON()
+
     .toss()
